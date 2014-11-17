c6d6dce2a44405cd71a19e23d726d0bcf6d98f59
Bogo <k@bogotobogo.com>
2014-11-16 19:04:47 -0800

    test added

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -5,5 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
-
+    @Test
+    public void testFunds() {
+       BankAccount account = new BankAccount(10);
+       double amount = account.debit(5);
+       Assert.assertEquals(5.0, amount);
 }
