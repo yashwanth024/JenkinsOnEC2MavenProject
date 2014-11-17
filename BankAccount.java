a86a2e77c3a335ebdeb3f80b9973fa75fa61b9d4
Bogo <k@bogotobogo.com>
2014-11-16 19:21:56 -0800

    bogo

--- BankAccount.java
+++ BankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package com.bogotobogo;
 
 public class BankAccount {
 
--- TestBankAccount.java
+++ TestBankAccount.java
@@ -1,4 +1,4 @@
-package ca.uwo.csd.cs2212.USERNAME;
+package bogotobogo.com;
 
 import junit.framework.Assert;
 import org.junit.Before;
