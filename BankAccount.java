b527594d3fe53b2509ac4a38497c98614e7be873
Bogo <k@bogotobogo.com>
2014-11-16 18:57:42 -0800

    original files

--- BankAccount.java
+++ BankAccount.java
@@ -1,10 +1,10 @@
-package com.bogotobogo;
+package ca.uwo.csd.cs2212.USERNAME;
 
-public class Account {
+public class BankAccount {
 
   private double balance;
 
-  public Account(double balance) {
+  public BankAccount(double balance) {
     this.balance = balance;
   }
 
